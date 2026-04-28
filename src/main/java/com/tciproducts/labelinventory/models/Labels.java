56f87ce18df62126ee031733efcb95e0d02a42a8
Chris <christopherschwartz559@gmail.com>
2026-04-28 08:57:58 -0500

    model and repo fleshed out

--- src/main/java/com/tciproducts/labelinventory/models/Labels.java
+++ src/main/java/com/tciproducts/labelinventory/models/Labels.java
@@ -1,13 +1,30 @@
 package com.tciproducts.labelinventory.models;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Labels extends AbstractEntity {
 
+    @Size(min = 3, max = 5, message = "code needs to be between 3 and 5 digits")
     private Long labelCode;
+
 
     private String labelAlias;
 
+    @Size(max = 2, message = "label size needs to be 2 characters")
     private String labelSize;
+
+    @Nullable
+    private Integer quantity;
+
+    @Override
+    public String toString() {
+        return labelSize;
+    }
 }
